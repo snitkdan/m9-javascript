ebace70d2de6f0c46b0d4e44db529037a6f4c6a6
Daniel Snitkovskiy <Daniel@D-69-91-144-151.dhcp4.washington.edu>
2016-10-11 10:17:00 -0700

    Add exercise3

--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -3,10 +3,14 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
 document.addEventListener("DOMContentLoaded", function(event) {
   // Create a new input element
-
+  var input = document.createElement('input');
 	// Append the new input element to you DOM in the desired location
-
+  var input_p = document.getElementById('input-p');
+  input_p.appendChild(input);
 	// Assign a click event to your button that:
+  document.getElementById('my-btn').addEventListener('click', function(){
+
+  });
 		// Creates a new paragraph of text from your input element, and:
 		// Clears the text of the input element
 });
