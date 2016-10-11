70be04d711785dbbaf800e727a3c5048e25b210f
Daniel Snitkovskiy <Daniel@D-69-91-144-151.dhcp4.washington.edu>
2016-10-11 09:48:11 -0700

    Add exercise-2

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -4,21 +4,27 @@
 var arr1 = [1,2,3,4,5];
 
 // Write a function to subtract one from a value
-
-
+var subtractOne = function(value) {
+	return value - 1;
+}
 // Pass each value to the subtractOne function by using the map functionality
+console.log("arr1 before: " + arr1);
+arr2 = arr1.map(subtractOne);
+console.log("arr1 subtractOne: " + arr2);
 
 
 // Create a array equal to arr1 times two by using the inline "map" funcitonality
+arr3 = arr1.map(function(value){
+	return value * 2;
+});
+console.log("arr1 times2: " + arr3);
 
-
-// Array of strings
-var names = ['Tim', 'Nick', 'Grant'];
 
 // Write an introduction function that takes an argument and returns "hello, I'm " + argument
-
-
-// Create an array of introductions for each name
+var intro = function(arg) {
+	return "Hello, I'm " + arg;
+}
+console.log(intro("Daniel"));
 
 // Array of objects
 var people = [
@@ -27,5 +33,15 @@ var people = [
 	{name:'Isabel', age:43}
 ];
 
+// Create an array of introductions for each name
+intro_names = new Array();
+for (i = 0; i < people.length; i++){
+	intro_names.push(intro(people[i]['name']));
+}
+
 // Use the `.forEach` method to create a new property `intro` for each object in your people array
 // The property should say "My name is NAME and I'm AGE years old"
+people.forEach(function(x){
+	x.intro = "My name is " +  x.name + "and I'm " + x.age + " years old";
+});
+console.log(people);
